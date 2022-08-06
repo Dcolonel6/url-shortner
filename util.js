f0cce181402ee883a98d332cb933c84fb9ab53a6
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-06 13:19:40 +0300

    added a fn to send data to url shortner service

--- util.js
+++ util.js
@@ -15,3 +15,35 @@ returns an array
 function grabContentFromCli(inputs){
     return argv.slice(2);    
 }
+/**
+ * post a payload to URLShortner service
+ * 
+ * @param {*string} urlToShorten 
+ * @param {*object payload 
+ * @param {*string} access_token 
+ */
+function sendToShortnerService(urlToShorten,payload,access_token=process.env.API_TOKEN){
+
+    axios.post(`${BASE_URL}/create`,
+        {
+            data: payload
+        },
+        {
+            headers:{
+                'Authorization': `Basic ${access_token}` 
+            }
+
+        })
+    .then((res) =>{
+        console.log(res.data);
+    })
+    .catch((error) =>{
+        console.error(error);
+    })
+
+}
+
+export default {
+    grabInputFromCLI: grabContentFromCli,
+    postToShortner: sendToShortnerService
+};
